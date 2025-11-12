6329a935208d258041dafeb4a70c862d6ccfc96c
amaan-malik1 <amikm2077@gmail.com>
2025-11-12 11:53:21 +0530

    Added postgresql db

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,22 +1,3 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
+import { PrismaClient } from '@prisma/client';
 
-dotenv.config();
-
-const MONGO_URI = process.env.MONGO_URI as string;
-
-export async function connectDB() {
-    try {
-        if (!MONGO_URI) {
-            console.error("❌ MongoDB connection failed: MONGO_URI is not defined in .env");
-            process.exit(1);
-        }
-
-        await mongoose.connect(MONGO_URI);
-
-        console.log("✅ MongoDB connected successfully");
-    } catch (error) {
-        console.error("❌ Error connecting to MongoDB:", error);
-        process.exit(1);
-    }
-}
+export const prismaClient = new PrismaClient(); 
